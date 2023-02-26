cd84ea072a3af9ad9c8ef5c6c896727e7ecfb483
Taras Samoilenko <tsamoilenko97@gmail.com>
2023-02-27 00:29:13 +0200

    hm

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-new Date();
+// Feb Sat 25
+
+// const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// const months = [
+//   "Jan",
+//   "Feb",
+//   "Mar",
+//   "Apr",
+//   "May",
+//   "Jun",
+//   "Jul",
+//   "Aug",
+//   "Sep",
+//   "Oct",
+//   "Nov",
+//   "Dec",
+// ];
+// const printDay = (date) => {
+//   const month = date.getMonth();
+//   const weekDay = date.getDay();
+//   const day = date.getDate();
+//   return `${months[month]} ${days[weekDay]} ${day}`;
+// };
+
+// console.log(printDay(new Date("2021-2-24")));
+
+// const map = new Map();
