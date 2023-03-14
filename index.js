d1f92fb470f585fbbb2310fa54f9c7ae77e4fa78
Taras Samoilenko <tsamoilenko97@gmail.com>
2023-03-14 13:23:32 +0200

    hm code

--- index.js
+++ index.js
@@ -1,3 +1,122 @@
+class Vehicle {
+  constructor({ power, gasTank, mass }) {
+    this.power = power;
+    this.gasTank = gasTank;
+    this.gas = gasTank;
+    this.mass = mass;
+    this.#maxSpeed = this.#calculateMaxSpeed();
+    this.#gasUsage = this.#calculateGasUsage();
+  }
+  started = false;
+  speed = 0;
+  #maxSpeed = 0;
+  #gasUsage = 0;
+  #speedMultiplier = 0.84;
+  #calculateMaxSpeed = () => {
+    return (this.#speedMultiplier * this.power) / this.mass;
+  };
+  #calculateGasUsage = () => {
+    return Math.round((this.#maxSpeed / this.power) * 100);
+  };
+  #calculateUsedGas = (distance) => {
+    return (distance * this.#gasUsage) / 100;
+  };
+  getMaxSpeed = () => {
+    return this.#maxSpeed;
+  };
+  getGasUsage = () => {
+    return this.#gasUsage;
+  };
+  startEngine = () => {
+    this.started = true;
+    return this;
+  };
+  stopEngine = () => {
+    this.started = false;
+    return this;
+  };
+  drive = (speed, distance) => {
+    const gasUsed = this.#calculateUsedGas(distance);
+    const gasDiff = this.gas - gasUsed;
+    if (this.started) {
+      if (speed > this.#maxSpeed) {
+        this.speed = this.#maxSpeed;
+      } else if (speed < 0) {
+        this.speed = 0;
+      } else {
+        this.speed = speed;
+      }
+      this.gas = gasDiff <= 0 ? 0 : gasDiff;
+    }
+    return this;
+  };
+
+  addGas = (gas) => {
+    const gasSum = this.gas + gas;
+    this.gas = gasSum > this.gasTank ? this.gasTank : gasSum;
+    return this;
+  };
+
+  printInfo() {
+    const info = {
+      power: this.power,
+      gasTank: this.gasTank,
+      gas: this.gas,
+      mass: this.mass,
+      maxSpeed: this.#maxSpeed,
+      gasUsage: this.#gasUsage,
+      started: this.started,
+      speed: this.speed,
+    };
+    console.log(info);
+    return info;
+  }
+}
+
+class Car extends Vehicle {
+  constructor({ type, maxPassengerCount, ...rest }) {
+    super(rest);
+    this.type = type;
+    this.maxPassengerCount = maxPassengerCount;
+  }
+  printInfo() {
+    const info = super.printInfo();
+    const updatedInfo = {
+      ...info,
+      type: this.type,
+      maxPassengerCount: this.maxPassengerCount,
+    };
+    console.log(updatedInfo);
+    return updatedInfo;
+  }
+}
+
+class Bus extends Car {
+  constructor(config) {
+    super(config);
+  }
+  passengerCount = 0;
+  updatePassengers = (passengers) => {
+    const passengersDiff = this.passengerCount + passengers;
+    if (passengersDiff > this.maxPassengerCount) {
+      this.passengerCount = this.maxPassengerCount;
+    } else if (passengersDiff < 0) {
+      this.passengerCount = 0;
+    } else {
+      this.passengerCount = passengersDiff;
+    }
+  };
+  printInfo() {
+    const info = super.printInfo();
+    const updatedInfo = {
+      ...info,
+      passengerCount: this.passengerCount,
+    };
+    console.log(updatedInfo);
+    return updatedInfo;
+  }
+}
+
 // class User {
 //   constructor(name) {
 //     this.name = name;
@@ -161,83 +280,83 @@
 // const decoratedColor = decorator(square.getColor);
 // decoratedColor();
 
-function addGas(gas) {
-  const gasVolume = this.gasTank + gas;
-  this.gasTank = gasVolume > this.maxGas ? this.maxGas : gasVolume;
+// function addGas(gas) {
+//   const gasVolume = this.gasTank + gas;
+//   this.gasTank = gasVolume > this.maxGas ? this.maxGas : gasVolume;
 
-  return this;
-}
+//   return this;
+// }
 
-class Car {
-  constructor(model, color, age, speed, gasTank, started) {
-    this.model = model;
-    this.color = color;
-    this.age = age;
-    this.#speed = speed;
-    this.#gasTank = gasTank;
-    this.#started = started;
-  }
-  #speed;
-  #gasTank;
-  #started;
-  #maxSpeed = 200;
-  #maxGas = 20;
-  #checkIsEmptyGasTask = () => {
-    return this.gasTank === 0;
-  };
-  startEngine = () => {
-    this.#started = !this.#checkIsEmptyGasTask();
+// class Car {
+//   constructor(model, color, age, speed, gasTank, started) {
+//     this.model = model;
+//     this.color = color;
+//     this.age = age;
+//     this.#speed = speed;
+//     this.#gasTank = gasTank;
+//     this.#started = started;
+//   }
+//   #speed;
+//   #gasTank;
+//   #started;
+//   #maxSpeed = 200;
+//   #maxGas = 20;
+//   #checkIsEmptyGasTask = () => {
+//     return this.gasTank === 0;
+//   };
+//   startEngine = () => {
+//     this.#started = !this.#checkIsEmptyGasTask();
 
-    return this;
-  };
-  drive() {
-    const isEmpty = this.#checkIsEmptyGasTask();
-    const isStarted = this.#started;
-    if (!isEmpty && isStarted) {
-      this.#speed = 30;
-    }
+//     return this;
+//   };
+//   drive() {
+//     const isEmpty = this.#checkIsEmptyGasTask();
+//     const isStarted = this.#started;
+//     if (!isEmpty && isStarted) {
+//       this.#speed = 30;
+//     }
 
-    return this;
-  }
-  stop = () => {
-    this.#started = false;
-    this.#speed = 0;
+//     return this;
+//   }
+//   stop = () => {
+//     this.#started = false;
+//     this.#speed = 0;
 
-    return this;
-  };
-  speedUp = (newSpeed) => {
-    const isEmpty = this.#checkIsEmptyGasTask();
-    const isStarted = this.#started;
+//     return this;
+//   };
+//   speedUp = (newSpeed) => {
+//     const isEmpty = this.#checkIsEmptyGasTask();
+//     const isStarted = this.#started;
 
-    if (!isEmpty && isStarted) {
-      const gasVolume = this.gasTank - 5;
-      this.#speed =
-        this.#speed + newSpeed > this.#maxSpeed ? this.#maxSpeed : newSpeed;
-      this.#gasTank = gasVolume < 0 ? 0 : gasVolume;
-    } else {
-      this.stop();
-    }
+//     if (!isEmpty && isStarted) {
+//       const gasVolume = this.gasTank - 5;
+//       this.#speed =
+//         this.#speed + newSpeed > this.#maxSpeed ? this.#maxSpeed : newSpeed;
+//       this.#gasTank = gasVolume < 0 ? 0 : gasVolume;
+//     } else {
+//       this.stop();
+//     }
 
-    return this;
-  };
-  slowDown = (newSpeed) => {
-    this.speedUp(newSpeed);
+//     return this;
+//   };
+//   slowDown = (newSpeed) => {
+//     this.speedUp(newSpeed);
 
-    return this;
-  };
-  addGas = (gas) => {
-    const gasVolume = this.#gasTank + gas;
-    this.#gasTank = gasVolume > this.#maxGas ? this.#maxGas : gasVolume;
+//     return this;
+//   };
+//   addGas = (gas) => {
+//     const gasVolume = this.#gasTank + gas;
+//     this.#gasTank = gasVolume > this.#maxGas ? this.#maxGas : gasVolume;
 
-    return this;
-  };
-}
+//     return this;
+//   };
+// }
 
-const car = new Car("Tesla", "red", 3, 0, 10, false);
+// const car = new Car("Tesla", "red", 3, 0, 10, false);
 
-class Bus extends Car {
-  constructor(model, color, age, speed, gasTank, started, maxPassenger) {
-    super(model, color, age, speed, gasTank, started);
-    this.maxPassenger = maxPassenger;
-  }
-}
+// class Bus extends Car {
+//   constructor(model, color, age, speed, gasTank, started, maxPassenger) {
+//     super(model, color, age, speed, gasTank, started);
+//     this.maxPassenger = maxPassenger;
+//   }
+// }
--- readme.md
+++ readme.md
@@ -9,14 +9,15 @@ Create class **Vehicle**
 Initialize class with properties **power**, **gasTank** and **mass** in tones
 Calculate max speed by formula **0.84 \* power / mass**  
 Create method **getMaxSpeed** that returns **maxSpeed**  
-Calculate gas usage per km by formula **maxSpeed / power**  
+Calculate gas usage per km by formula **Math.round(maxSpeed / power \* 100)**  
 Create method **getGasUsage** that returns **gasUsage**
 Create method **startEngine** that sets property **started** to true
 Create method **stopEngine** that sets property **started** to false
-Create method **drive**. This method receives 2 arguments **speed** and **distance**  
+Create method **drive**. This method receives 2 arguments **speed** and **distance** in km  
 You cannot **drive** if **started** is false  
 You cannot set **speed** more then **maxSpeed** and less then 0  
 Update **gas** property corresponding to **gasUsage**
+Formula to calculate **gas** level is **distance** \* **gasUsage** / 100  
 Create method **addGas** adds **gas** to car by arg  
 Argument must be bigger then zero  
 You cannot pour more **gas** then **gasTank**  
@@ -29,7 +30,7 @@ Examples of **type** SEDAN, MINIVAN, SPORTS CAR...
 Update method **printInfo** that prints in console all available information
 
 Create class **Bus**  
-**Bus** should inherit from **Car**  
+**Bus** should inherit from **Car**
 Create method **updatePassengers** that receives argument **passengers** and updates **passengerCount** to that number  
 **passengerCount** cannot be more then **maxPassengerCount** and less then 0  
 Update method **printInfo** that prints in console all available information
