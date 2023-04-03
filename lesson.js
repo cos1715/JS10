67d0b1076666771343ca0fa377eeadcb4f93d304
Taras Samoilenko <tsamoilenko97@gmail.com>
2023-04-03 13:20:03 +0300

    lesson

--- lesson.js
+++ lesson.js
@@ -1,76 +1,111 @@
-const btn = document.getElementById("btn");
-const root = document.getElementById("root");
-const div = document.getElementById("inner-div");
+const form = document.getElementById("form");
+// const firstName = form.elements["firstName"];
+// const checkbox = form.elements["checkbox"];
+// console.dir(form);
 
-const onClick = (type) => (event) => {
-  event.stopPropagation();
-  console.log(type, event);
-};
+// checkbox.addEventListener("change", (e) => {
+//   console.log(e);
+// });
 
-btn.addEventListener("mousedown", (event) => {
-  console.log("mousedown", event);
-});
-btn.addEventListener("mouseup", (event) => {
-  console.log("mouseup", event);
-});
-btn.addEventListener("click", (event) => {
-  console.log("click", event);
-});
-// root.addEventListener("click", onClick("root div"));
-// div.addEventListener("click", onClick("inner div"));
+// const radioButtons = document.querySelectorAll("input[type='radio']");
+// console.log(radioButtons);
+// for (let radioBtn of radioButtons) {
+//   radioBtn.addEventListener("change", (e) => {
+//     console.log(e.target.checked, e.target.value);
+//   });
+// }
 
-// root.addEventListener("click", (event) => {
-//   console.log(event.target.nodeName, event.target.id);
-//   if (event.target.nodeName === "BUTTON") {
-//     event.target.innerText = "Clicked!!!";
-//   }
-//   if (event.target.nodeName === "DIV") {
-//     let color = "#";
-//     for (let i = 0; i < 3; i++) {
-//       color += Math.floor(Math.random() * 256).toString(16);
-//     }
-//     event.target.style.background = color;
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   console.log(e);
+//   const formData = new FormData(e.target);
+//   const entries = formData.entries();
+
+//   for (let entry of entries) {
+//     console.log(`name: ${entry[0]}`, `value: ${entry[1]}`);
 //   }
 // });
 
-// elem.removeEventListener("click", onClick);
+// firstName.addEventListener("focus", (event) => {
+//   console.log("focus", event.target.value);
+// });
 
-// const btn = document.getElementById("btn");
-// btn.addEventListener("click", (e) => {
-//   e.target.innerHTML = "Clicked!!!";
-//   let color = "#";
-//   for (let i = 0; i < 3; i++) {
-//     color += Math.floor(Math.random() * 256).toString(16);
+// firstName.addEventListener("keydown", (event) => {
+//   console.log("keydown", event.target.value);
+//   console.log("keydown", event);
+//   // key
+// });
+
+// firstName.addEventListener("keyup", (event) => {
+//   const value = event.target.value.toLowerCase();
+//   const placeholder = event.target.placeholder.toLowerCase();
+//   const placeholderArr = placeholder.split("");
+//   const index = value.length - 1;
+//   const lastLetter = value.slice(-1);
+//   console.log("keyup", event.target.placeholder);
+//   if (placeholderArr[index] !== lastLetter) {
+//     const correctValue = value.slice(0, index);
+//     event.target.value = correctValue;
 //   }
-//   e.target.style.background = color;
 // });
 
-// const collapseTitle = document.getElementById("collapse-title");
-// collapseTitle.addEventListener("click", (event) => {
-//   console.log(event);
-// });
-
-// const text = document.getElementById("text");
-// const textState = text.innerText;
-
-// text.addEventListener("click", (event) => {
-//   const collapsed = event.target.getAttribute("data-collapsed");
-//   if (collapsed === "true") {
-//     event.target.innerText = textState;
-//     event.target.setAttribute("data-collapsed", false);
+// firstName.addEventListener("blur", (event) => {
+//   console.log("blur", event);
+//   if (event.target.value.length > 10) {
+//     event.target.style.border = "1px solid green";
 //   } else {
-//     const slicedText = textState.slice(0, 25);
-//     event.target.innerText = `${slicedText}...`;
-//     event.target.setAttribute("data-collapsed", true);
+//     event.target.style.border = "1px solid red";
+//   }
+//   // event.target.value = "Taras";
+// });
+
+// const password = document.getElementById("password");
+// const btn = document.getElementById("btn");
+
+// btn.addEventListener("click", (e) => {
+//   if (password.type === "password") {
+//     e.target.innerText = "Hide password";
+//     password.type = "text";
+//   } else {
+//     password.type = "password";
+//     e.target.innerText = "Show password";
 //   }
 // });
 
-const table = document.getElementById("table");
-table.addEventListener("click", (e) => {
-  if (e.target.tagName === "TH") {
-    alert(e.target.innerText);
-  }
-  if (e.target.tagName === "TD") {
-    console.log(e.target.innerText);
-  }
+// const fileInput = document.getElementById("file");
+
+// fileInput.addEventListener("change", (e) => {
+//   console.log(e);
+//   const reader = new FileReader();
+
+//   reader.addEventListener("load", (event) => {
+//     console.log("load event", event);
+//     const img = document.createElement("img");
+//     img.src = reader.result;
+//     console.log("result", reader.result);
+//     document.body.append(img);
+//   });
+
+//   reader.readAsDataURL(e.target.files[0]);
+// });
+
+const fileInput = document.getElementById("file");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "multipart/form-data");
+  myHeaders.append(
+    "x-api-key",
+    "live_KM2XaepFFn85vLPmcyoNXg6HNdCzsJHhSDYH83X5JxdNBrv8zlqTRCFPrQUdVJEp"
+  );
+
+  const formData = new FormData();
+  formData.append("file", fileInput.files[0]);
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: formData,
+  };
+  fetch("https://api.thecatapi.com/v1/images/upload", requestOptions);
 });
--- readme.md
+++ readme.md
@@ -1,22 +1,3 @@
 # JS Homework
 
-## Click event
-
-Create ul element and 10 li elements inside  
-Add event listener to ul  
-Highlight li on click  
-If clicked on same li remove highlight  
-If clicked on another li add highlight to new li and remove from prev li  
-If clicked with ctrl highlight prev li and new li  
-if clicked with shift highlight all li from prev li to new li
-
-P.S. This logic should be similar to windows folder select
-P.P.S. Continue work on project
-
-## Pointer
-
-Add click event listener  
-On click create dot shaped element  
-Dot element should appear on click place
-
-P.S. Explore event object
+Continue work on module project
